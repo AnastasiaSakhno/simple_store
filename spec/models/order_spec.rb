f1f3081f66951825a0026c26652b45153cad3f8e
AnastasiaSakhno <anastasia.sakhno@gmail.com>
2012-09-10 22:35:34 +0300

    refactored order test about customer items count

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -7,10 +7,8 @@ describe "Order" do
   it "should give items the customer bought" do
     customer = FactoryGirl.create(:customer)
     item = FactoryGirl.create(:item)
-    orders = []
     count = 5
-    count.times { orders << Order.new(:customer_id => customer.id, :item_id => item.id) }
-    orders.each { |order| order.save }
+    count.times { Order.new(:customer_id => customer.id, :item_id => item.id).save }
     Order.find_by_customer(customer.id).size.should == count
   end
 end
